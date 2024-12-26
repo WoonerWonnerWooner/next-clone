afd322a3f897288b842a76479fce686aa742df53
WoonerWoonerWooner <wovkfdl45645@gmail.com>
2024-12-26 16:56:25 +0900

    메인 Edit 리스트 가져오기

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,24 @@
-import Image from "next/image";
+import Post from "@/components/posts/editor/Post";
+import PostEditor from "@/components/posts/editor/PostEditor";
+import TrendsSidebar from "@/components/TrendsSidebar";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: {createdAt: "desc" }
+  });
+
   return (
-    <main>Front page</main>
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+      <TrendsSidebar />
+    </main>
   );
 }
