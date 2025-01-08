04a0fdf6eb1be3759a674f72b886d131b90171b2
WoonerWoonerWooner <wovkfdl45645@gmail.com>
2025-01-08 09:26:40 +0900

    툴팁 작업

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -2,51 +2,57 @@
 
 import { useSession } from "@/app/(main)/SessionProvider";
 import UserAvatar from "@/components/UserAvatar";
-import { PostData } from "@/lib/types"
+import { PostData } from "@/lib/types";
 import { formatRelativeDate } from "@/lib/utils";
 import Link from "next/link";
 import PostMoreButton from "./PostMoreButton";
+import Linkify from "../Linkify";
+import UserTooltip from "../UserTooltip";
 
 interface PostProps {
-    post: PostData;
+  post: PostData;
 }
 
-export default function Post({ post }:PostProps) {
+export default function Post({ post }: PostProps) {
+  const { user } = useSession();
 
-    const {user} = useSession();
+  return (
+    <article className="group/post space-y-3 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="flex justify-between gap-3">
+        <div className="flex flex-wrap gap-3">
+          <UserTooltip user={post.user}>
+            <Link href={`/users/${post.user.username}`}>
+              <UserAvatar avatarUrl={post.user.avatarUrl} />
+            </Link>
+          </UserTooltip>
 
-    return (
-        <article className="group/post space-y-3 rounded-2xl bg-card p-5 shadow-sm">
-            <div className="flex justify-between gap-3">
-                <div className="flex flex-wrap gap-3">
-                    <Link href={`/users/${post.user.username}`}>
-                        <UserAvatar avatarUrl={post.user.avatarUrl} />
-                    </Link>
-                        <div>
-                        <Link 
-                            href={`/users/${post.user.username}`}
-                            className="block font-medium hover:underline"
-                        >
-                            {post.user.username}
-                        </Link>
-                        <Link
-                            href={`/posts/${post.id}`}
-                            className="blcok text-sm text-muted-foreground hover:underline"
-                        >
-                            {formatRelativeDate(post.createdAt)}
-                        </Link>
-                    </div>
-                </div>
-                {post.user.id === user.id && (
-                    <PostMoreButton 
-                        post={post} 
-                        className="opacity-0 transition-opacity group-hover/post:opacity-100" 
-                    />
-                )}
-            </div>
-            <div className="whitespace-pre-line break-words">
-                {post.content}
-            </div>
-        </article>
-    )
+          <div>
+            <UserTooltip user={post.user}>
+              <Link
+                href={`/users/${post.user.username}`}
+                className="block font-medium hover:underline"
+              >
+                {post.user.username}
+              </Link>
+            </UserTooltip>
+            <Link
+              href={`/posts/${post.id}`}
+              className="blcok text-sm text-muted-foreground hover:underline"
+            >
+              {formatRelativeDate(post.createdAt)}
+            </Link>
+          </div>
+        </div>
+        {post.user.id === user.id && (
+          <PostMoreButton
+            post={post}
+            className="opacity-0 transition-opacity group-hover/post:opacity-100"
+          />
+        )}
+      </div>
+      <Linkify>
+        <div className="whitespace-pre-line break-words">{post.content}</div>
+      </Linkify>
+    </article>
+  );
 }
